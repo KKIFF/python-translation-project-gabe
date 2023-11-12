10d8c2de1ea4e77be43669f6be9080f2af4f87c4
Gabe Nixon <D00447126@utahtech.edu>
2023-11-12 13:42:10 -0700

    Adding translate_orf.py and a mostly working translate.py

---
+++ translate_orf.py
@@ -0,0 +1,26 @@
+import sys
+from find_orf import find_first_orf, parse_sequence_from_path
+from translate import translate_sequence
+genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P', 'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C', 'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S', 'AGG': 'R', 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*', 'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I', 'GCA': 'A', 'CUU': 'L', 'GGC': 'G', 'AUG': 'M', 'CUG': 'L', 'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A', 'AAA': 'K', 'AAG': 'K', 'CAA': 'Q', 'UUU': 'F', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I', 'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F' }
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: ./translate_orf.py <path_to_sequence_file>")
+        sys.exit(1)
+    sequence_path = sys.argv[1]
+    try:
+        sequence = parse_sequence_from_path(sequence_path)
+    except FileNotFoundError:
+        print(f"Error: File not found: {sequence_path}")
+        sys.exit(1)
+    orf = find_first_orf(sequence)
+    if not orf:
+        print("No valid ORF found.")
+        sys.exit(1)
+
+    amino_acids = translate_sequence(orf, genetic_code)
+    print("Translated Amino Acid Sequence:")
+    print(amino_acids)
+
+if __name__ == "__main__":
+    main()
